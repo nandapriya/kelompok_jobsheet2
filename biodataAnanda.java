603810484d3c922cf2041cb529935ccbdcde02b9
nandapriya <162028427+nandapriya@users.noreply.github.com>
2024-09-08 19:54:12 +0700

    initial commit

---
+++ biodataAnanda.java
@@ -0,0 +1,15 @@
+public class biodataAnanda {
+    public static void main(String[] args) {
+        System.out.println("===============================================");
+        System.out.println("\t\t BIODATA DIRI \t\t");
+        System.out.println("===============================================");
+        System.out.println("");
+        System.out.println("Nama \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+    }
+}
